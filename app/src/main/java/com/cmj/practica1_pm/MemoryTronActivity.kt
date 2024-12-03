4975f6ca54998f2bc49e9a8c93ed71b4ec263bcf
Nozferatu <sparrowhawk2005@gmail.com>
2024-12-03 19:04:02 +0100

    Imports residuales

--- app/src/main/java/com/cmj/practica1_pm/MemoryTronActivity.kt
+++ app/src/main/java/com/cmj/practica1_pm/MemoryTronActivity.kt
@@ -18,11 +18,7 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
