c7fd48ea732f20f53585cf1db8a56442d9eac809
Nozferatu <sparrowhawk2005@gmail.com>
2024-12-03 19:03:42 +0100

    Mecánica de volteo terminada

--- app/src/main/java/com/cmj/practica1_pm/MemoryTronActivity.kt
+++ app/src/main/java/com/cmj/practica1_pm/MemoryTronActivity.kt
@@ -1,35 +1,40 @@
 package com.cmj.practica1_pm
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.integerResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cmj.practica1_pm.ui.theme.Practica1PMTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private var cartasEscogidas = mutableMapOf<Int, Int>() //Indice - Imagen
+private var elecciones = mutableListOf(-1, -1)
+private val cartasVolteadas = mutableStateListOf<Boolean>()
 
 class MemoryTronActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,9 +48,6 @@ class MemoryTronActivity : ComponentActivity() {
         )
 
         setContent {
-            val cartasEscogidas = remember { mutableStateMapOf<Int, Int>() } //Indice - Imagen
-            var cartasVolteadas = remember { mutableStateListOf<Boolean>() }
-
             var indice = 0
             var imagenCarta: Int
             var imagenValida: Boolean
@@ -75,9 +77,7 @@ class MemoryTronActivity : ComponentActivity() {
                 Surface {
                     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                         MemoryTron(
-                            innerPadding,
-                            cartasEscogidas,
-                            cartasVolteadas
+                            innerPadding
                         )
                     }
                 }
@@ -86,21 +86,59 @@ class MemoryTronActivity : ComponentActivity() {
     }
 }
 
+suspend fun comprobarElecciones(cartasVolteadas: MutableList<Boolean>){
+    val eleccion1 = cartasEscogidas[elecciones[0]]
+    val eleccion2 = cartasEscogidas[elecciones[1]]
+
+    Log.d("Cartas", "$eleccion1 - $eleccion2")
+
+    if(eleccion1 != eleccion2){
+        Log.d("Cartas", "No son iguales, se voltean de vuelta")
+        delay(1000L)
+
+        cartasVolteadas[elecciones[0]] = false
+        cartasVolteadas[elecciones[1]] = false
+    }
+
+    elecciones[0] = -1
+    elecciones[1] = -1
+}
+
 
 @Composable
-fun Carta(index: Int, volteada: Boolean, imagen: Int){
+fun Carta(index: Int, imagenVolteada: Int){
+    val coroutineScope = rememberCoroutineScope()
+
+    val imagenActual = when(cartasVolteadas[index]){
+        true -> imagenVolteada
+        false -> R.drawable.reverso_naipe
+    }
+
     Image(
-        painterResource(imagen),
+        painterResource(imagenActual),
         "Carta",
         modifier = Modifier
-            .padding(10.dp),
+            .padding(10.dp)
+            .clickable {
+                cartasVolteadas[index] = true
+
+                if(elecciones[0] == -1){
+                    Log.d("Cartas", "Primera elección: $index")
+                    elecciones[0] = index
+                }else if(elecciones[0] != index && elecciones[1] == -1){
+                    Log.d("Cartas", "Segunda elección: $index")
+                    elecciones[1] = index
+
+                    coroutineScope.launch {
+                        comprobarElecciones(cartasVolteadas)
+                    }
+                }
+            }
     )
 }
 
 @Composable
-fun MemoryTron(innerPadding: PaddingValues, cartasEscogidas: Map<Int, Int>, cartasVolteadas: List<Boolean>) {
-    var eleccion1 = rememberSaveable { mutableStateOf("") }
-
+fun MemoryTron(innerPadding: PaddingValues) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
         modifier = Modifier.wrapContentHeight()
@@ -112,7 +150,7 @@ fun MemoryTron(innerPadding: PaddingValues, cartasEscogidas: Map<Int, Int>, cart
             val indice = it.first
             val imagenCarta = it.second
 
-            Carta(indice, cartasVolteadas[indice], imagenCarta)
+            Carta(indice, imagenCarta)
         }
     }
 }
