b66d059c952ceaf557a806234f14963288595ef6
Nozferatu <sparrowhawk2005@gmail.com>
2024-12-14 14:25:18 +0100

    Revert "Actividad de la configuración"
    This reverts commit ef13fce4f3a59807d6092345fc9303c9c99e8e4c.

--- app/src/main/java/com/cmj/practica1_pm/CalculaTronActivity.kt
+++ app/src/main/java/com/cmj/practica1_pm/CalculaTronActivity.kt
@@ -1,6 +1,5 @@
 package com.cmj.practica1_pm
 
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,23 +9,21 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.staggeredgrid.LazyHorizontalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -44,12 +41,8 @@ import com.cmj.practica1_pm.ui.theme.Practica1PMTheme
 import kotlinx.coroutines.delay
 
 
-private lateinit var sharedPreferences: SharedPreferences
-
 private val operandoA = mutableIntStateOf(0)
 private val operandoB = mutableIntStateOf(0)
-private lateinit var valorMinimo: MutableIntState
-private lateinit var valorMaximo: MutableIntState
 private val posiblesOperaciones = listOf("+", "-", "*")
 private val operacion = mutableStateOf("+")
 
@@ -62,16 +55,14 @@ class CalculaTronActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        sharedPreferences = getSharedPreferences("CalculaTron", MODE_PRIVATE)
-        valorMinimo = mutableIntStateOf(sharedPreferences.getInt("valorMinimo", 1))
-        valorMaximo = mutableIntStateOf(sharedPreferences.getInt("valorMaximo", 20))
-
         setContent {
+            barajarCartas()
+
             Practica1PMTheme {
                 Surface {
                     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                         CalculaTron(
-                            Modifier.padding(innerPadding)
+                            innerPadding
                         )
                     }
                 }
@@ -81,8 +72,8 @@ class CalculaTronActivity : ComponentActivity() {
 }
 
 fun generarOperacion(){
-    operandoA.intValue = (valorMinimo.intValue..valorMaximo.intValue).random()
-    operandoB.intValue = (valorMinimo.intValue..valorMaximo.intValue).random()
+    operandoA.intValue = (1..20).random()
+    operandoB.intValue = (1..20).random()
     operacion.value = posiblesOperaciones.random()
 }
 
@@ -119,7 +110,8 @@ fun Contador(contador: MutableState<Int>){
 fun Estadisticas(){
     Text(
         "Aciertos: ${aciertos.intValue} Fallos: ${fallos.intValue}",
-        fontSize = 20.sp
+        fontSize = 20.sp,
+        modifier = Modifier.padding(vertical = 20.dp)
     )
 }
 
@@ -133,7 +125,7 @@ fun Operacion(){
     ){
         Text("${operandoA.intValue} ${operacion.value} ${operandoB.intValue} = ?")
         OutlinedTextField(modifier = Modifier
-            .size(width = 100.dp, height = 40.dp),
+            .size(width = 100.dp, height = 50.dp),
             value = respuestaOperacion.value,
             onValueChange = { respuestaOperacion.value = it },
             readOnly = true
@@ -144,15 +136,15 @@ fun Operacion(){
 @Composable
 fun Tecla(
     contenido: String,
-    altura: Dp = 40.dp,
-    anchura: Dp = 40.dp,
+    altura: Dp = 50.dp,
+    anchura: Dp = 50.dp,
     funcion: () -> (Unit) = {
         respuestaOperacion.value += contenido
     }
 ){
     Box(modifier = Modifier
         .size(width = anchura, height = altura)
-        .padding(2.dp)
+        .padding(4.dp)
         .background(Color.Black)
         .clickable { funcion() },
         contentAlignment = Alignment.Center
@@ -169,64 +161,55 @@ fun Tecla(
 
 @Composable
 fun Teclado(){
-    LazyHorizontalStaggeredGrid(
+    LazyVerticalStaggeredGrid(
         modifier = Modifier
+            .width(200.dp)
             .wrapContentSize(),
-        rows = StaggeredGridCells.Fixed(4),
-        verticalArrangement = Arrangement.Center
+        columns = StaggeredGridCells.Fixed(4)
     ) {
         item{
-            Tecla("7")
+            Column {
+                Tecla("7")
+                Tecla("4")
+                Tecla("1")
+                Tecla("0", anchura = 100.dp)
+            }
         }
+
         item{
-            Tecla("4")
+            Column {
+                Tecla("8")
+                Tecla("5")
+                Tecla("2")
+                Tecla("C", funcion = { respuestaOperacion.value = respuestaOperacion.value.dropLast(1) })
+            }
         }
+
         item{
-            Tecla("1")
+            Column {
+                Tecla("9")
+                Tecla("6")
+                Tecla("3")
+                Tecla("-")
+            }
         }
+
         item{
-            Tecla("0")
-        }
-        item{
-            Tecla("8")
-        }
-        item{
-            Tecla("5")
-        }
-        item{
-            Tecla("2")
-        }
-        item{
-            Tecla("-")
-        }
-        item{
-            Tecla("9")
-        }
-        item{
-            Tecla("6")
-        }
-        item{
-            Tecla("3")
-        }
-        item{
-            Tecla("C", funcion = { respuestaOperacion.value = respuestaOperacion.value.dropLast(1) })
-        }
-        item{
-            Tecla("CE", funcion = { respuestaOperacion.value = "" })
-        }
-        item(span = StaggeredGridItemSpan.FullLine){
-            Tecla("=", funcion = { comprobarOperacion() })
+            Column {
+                Tecla("CE", funcion = { respuestaOperacion.value = "" })
+                Tecla("=", altura = 150.dp, funcion = { comprobarOperacion() })
+            }
         }
     }
 }
 
 @Composable
-fun CalculaTron(modifier: Modifier = Modifier) {
+fun CalculaTron(innerPadding: PaddingValues) {
     val contador = remember { mutableIntStateOf(20) }
 
-    Column(modifier = modifier
-        .fillMaxWidth()
-        .padding(bottom = 10.dp),
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -246,8 +229,8 @@ fun CalculaTron(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 900, heightDp = 400)
+@Preview(showBackground = true, widthDp = 400, heightDp = 900)
 @Composable
 fun CalculaTronPreview(){
-    CalculaTron()
+    CalculaTron(PaddingValues(20.dp))
 }
