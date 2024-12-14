b6fc1fda18c5d0f7c51c3f35a581135ecc949448
Nozferatu <sparrowhawk2005@gmail.com>
2024-12-14 14:25:58 +0100

    Revert "Añadido input"
    This reverts commit 9e860ae1b1ef52058bb60a51ac8472ea5e4346e8.

--- app/src/main/java/com/cmj/practica1_pm/CalculaTronActivity.kt
+++ app/src/main/java/com/cmj/practica1_pm/CalculaTronActivity.kt
@@ -6,11 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,16 +16,10 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,11 +28,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.cmj.practica1_pm.ui.theme.Practica1PMTheme
-import kotlinx.coroutines.delay
-
-private var respuestaOperacion = mutableStateOf("")
 
 class CalculaTronActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,44 +52,19 @@ class CalculaTronActivity : ComponentActivity() {
 }
 
 @Composable
-fun Contador(contador: MutableState<Int>){
-    Text(modifier = Modifier
-        .padding(vertical = 20.dp),
-        text = contador.value.toString(),
-        fontSize = 30.sp
-    )
-}
-
-@Composable
-fun Operacion(){
-    Row(modifier = Modifier
-        .width(200.dp)
-        .padding(vertical = 20.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween){
-        Text("2 + 2 = ?")
-        OutlinedTextField(modifier = Modifier
-            .size(width = 125.dp, height = 50.dp),
-            value = respuestaOperacion.value,
-            onValueChange = { respuestaOperacion.value = it }
-        )
-    }
-}
-
-@Composable
 fun Tecla(
     contenido: String,
     altura: Dp = 50.dp,
     anchura: Dp = 50.dp,
     funcion: () -> (Unit) = {
-        respuestaOperacion.value += contenido
+
     }
 ){
     Box(modifier = Modifier
         .size(width = anchura, height = altura)
         .padding(4.dp)
         .background(Color.Black)
-        .clickable { funcion() },
+        .clickable { funcion },
         contentAlignment = Alignment.Center
 
     ) {
@@ -118,8 +81,7 @@ fun Tecla(
 fun Teclado(){
     LazyVerticalStaggeredGrid(
         modifier = Modifier
-            .width(200.dp)
-            .wrapContentSize(),
+        .wrapContentSize(),
         columns = StaggeredGridCells.Fixed(4)
     ) {
         item{
@@ -136,7 +98,7 @@ fun Teclado(){
                 Tecla("8")
                 Tecla("5")
                 Tecla("2")
-                Tecla("C", funcion = { respuestaOperacion.value = respuestaOperacion.value.dropLast(1) })
+                Tecla("C")
             }
         }
 
@@ -160,29 +122,16 @@ fun Teclado(){
 
 @Composable
 fun CalculaTron(innerPadding: PaddingValues) {
-    val contador = remember { mutableIntStateOf(20) }
-
     Column(modifier = Modifier
-        .fillMaxSize()
-        .padding(innerPadding),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        .width(200.dp)
+        .padding(innerPadding)
     ) {
-        Contador(contador)
-        Operacion()
         Teclado()
-    }
-
-    LaunchedEffect(Unit) {
-        while(contador.intValue > 0){
-            delay(1000L)
-            contador.intValue--
-        }
     }
 }
 
-@Preview(showBackground = true, widthDp = 400, heightDp = 900)
+@Preview(showBackground = true)
 @Composable
-fun CalculaTronPreview(){
-    CalculaTron(PaddingValues(20.dp))
+fun TecladoPreview(){
+    Teclado()
 }
