a8f7c88ac830178a10cf5631a2611b7bc6e15ab8
Nozferatu <sparrowhawk2005@gmail.com>
2024-12-15 13:58:44 +0100

    Añadidos primeros inputs

--- app/src/main/java/com/cmj/practica1_pm/CalculaTronSettingsActivity.kt
+++ app/src/main/java/com/cmj/practica1_pm/CalculaTronSettingsActivity.kt
@@ -1,5 +1,6 @@
 package com.cmj.practica1_pm
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,15 +11,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.cmj.practica1_pm.ui.theme.Practica1PMTheme
+
+private lateinit var sharedPreferences: SharedPreferences
+
+private lateinit var valorMinimo: MutableIntState
+private lateinit var valorMaximo: MutableIntState
 
 class CalculaTronSettingsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        sharedPreferences = getSharedPreferences("CalculaTron", MODE_PRIVATE)
+        valorMinimo = mutableIntStateOf(sharedPreferences.getInt("valorMinimo", 1))
+        valorMaximo = mutableIntStateOf(sharedPreferences.getInt("valorMaximo", 20))
+
         setContent {
             Practica1PMTheme {
                 Surface {
@@ -35,10 +52,23 @@ class CalculaTronSettingsActivity : ComponentActivity() {
 
 @Composable
 fun Settings(modifier: Modifier = Modifier) {
+    var valorMinimoTemp by rememberSaveable { mutableIntStateOf(0) }
+    var valorMaximoTemp by rememberSaveable { mutableIntStateOf(0) }
+
     Column(modifier = modifier) {
-//        OutlinedTextField(
-//
-//        )
+        OutlinedTextField(
+            value =  valorMinimoTemp.toString(),
+            onValueChange = { valorMinimoTemp = it.toInt() },
+            label = { Text("Valor mínimo") },
+            singleLine = true
+        )
+
+        OutlinedTextField(
+            value =  valorMaximoTemp.toString(),
+            onValueChange = { valorMaximoTemp = it.toInt() },
+            label = { Text("Valor máximo") },
+            singleLine = true
+        )
     }
 }
 
